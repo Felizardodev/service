0109d74d929711f0ddb83c818dd461e53c6b44c6
Gustavo Santana <gustavofelipe68@gmail.com>
2023-10-17 22:48:38 -0300

    feature: Adicionado novas formas de fazer request e response

--- src/main/java/io/github/felizardoDev/Sum/SumController.java
+++ src/main/java/io/github/felizardoDev/Sum/SumController.java
@@ -4,6 +4,9 @@ import io.github.felizardoDev.Sum.SumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 
@@ -16,5 +19,24 @@ public class SumController {
     @ResponseBody
     public int soma (@PathVariable int num1, @PathVariable int num2) { //receber opr json, querystring
         return sumService.sum(num1, num2);
+    }
+
+    @GetMapping("/fazer-soma2")
+    @ResponseBody
+    public Map<String, Object> soma2 (@RequestParam int num1, @RequestParam int num2) {
+    // Mapa: <Tipo da Chave, Tipo do valor > -> <> indica Generico
+        Map<String, Object> rtn = new LinkedHashMap<>();
+        rtn.put("result", sumService.sum(num1, num2));
+        return rtn;
+    }
+
+    @GetMapping("/fazer-soma3")
+    @ResponseBody
+    public DataResponse<Integer> soma3 (@RequestParam int num1, @RequestParam int num2) {
+        Integer resultado = sumService.sum(num1, num2);
+
+        return DataResponse.<Integer>builder()
+                .result(resultado)
+                .build();
     }
 }
