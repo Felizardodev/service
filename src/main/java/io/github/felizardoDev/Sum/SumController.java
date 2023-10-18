5f39f56de28de3264a49e5e52e5497f61e973cb1
Felizardodev <suelenfelizardo@icloud.com>
2023-10-18 12:42:39 -0300

    Sexto commit alterações SUM

--- src/main/java/io/github/felizardoDev/Sum/SumController.java
+++ src/main/java/io/github/felizardoDev/Sum/SumController.java
@@ -15,12 +15,6 @@ public class SumController {
     @Autowired
     private SumService sumService;
 
-    @GetMapping("/make-sum/{num1}/{num2}") //no postman colocar o valor das var no path/ url
-    @ResponseBody
-    public int soma (@PathVariable int num1, @PathVariable int num2) { //receber opr json, querystring
-        return sumService.sum(num1, num2);
-    }
-
     @GetMapping("/make-sum2")
     @ResponseBody
     public Map<String, Object> soma2 (@RequestParam int num1, @RequestParam int num2) {
